1f1b60c3b76859e0092a13219259eedf71f06dcd
pedroarchilha <rm550450@fiap.com.br>
2023-04-28 21:01:50 -0300

    JS criado - Aula 28/04

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict"; // serve para a criacao de varivavel com typagem
+// console.log("Olá, Mundo!");
+
+// var nr1 = "10";
+// var nr2 = "5";
+
+
+// //realizando operacoes com Strings;
+
+// console.log(parseInt(nr1) + parseInt(nr2))
+
+
+// console.log(nr3)
+
+var name1 = "Dubas";
+// let name2 = "Serbatinho";
+// const name3 = "Julia";
+
+if(name1 == "")
+{
+    let name1 = "Julia"; // let serve como variavel local, 
+                         // respeitando o bloco.
+}
+
+console.log(name1);
