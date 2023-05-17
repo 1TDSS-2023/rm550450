efc3b41908fb59a7aa0a4649840e23887fe249c4
pedroarchilha <rm550450@fiap.com.br>
2023-05-17 19:48:31 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,25 +1,56 @@
-"use strict"; // serve para a criacao de varivavel com typagem
-// console.log("Olá, Mundo!");
+"use strict";
+// console.log("Olá Mundo!!");
 
 // var nr1 = "10";
 // var nr2 = "5";
 
+// //Realizando operações com Strings e realizando o parse com a
+// //função parseInt...;
+// console.log(parseInt(nr1)+parseInt(nr2));
 
-// //realizando operacoes com Strings;
+// var nome1 = "Dubas";
+// // let nome2 = "Serbatinho";
+// // const nome3 ="Julia";
+// nome = "Ale";
+// console.log(nome);
 
-// console.log(parseInt(nr1) + parseInt(nr2))
+// if(nome1 != ""){
+//     let nome1 = "Julia";
+// }
 
+// var nome
 
-// console.log(nr3)
+// console.log(nome1);
 
-var name1 = "Dubas";
-// let name2 = "Serbatinho";
-// const name3 = "Julia";
+/* <button value="click" id="meu-btn">Click</button> */
+const btn = document.getElementById("meu-btn");
 
-if(name1 == "")
-{
-    let name1 = "Julia"; // let serve como variavel local, 
-                         // respeitando o bloco.
-}
+//PRIMEIRO PASSO PARA A TRANSFORMACAO DO BOTAO EM RGB
 
-console.log(name1);
+//funcao addEventListener adiciona eventos ao elemento. Pode ser usado para adicionar qualquer evento a qualquer elemento, seja um botao criado, seja um h2 titulo de uma secao
+//function(){} eh uma funcao anonima
+btn.addEventListener("click", function () {
+    // click eh o evento esperado. Estao esta sendo adicionado o evento click. Quando alguem clicar no bota, ativara o q esta dentro da function
+    //console.log(btn.textContent); // aqui estamos atribuindo que mostrarah o conteudo texto do botao criado em HTML, no caso Click.
+
+    //random = retorna uma numero aleatorio entre 0 e 1
+    //floor = arredenda o numero pra baixo
+    //ceil = arredonda pra cima
+    //round = arredonda aleatorio
+
+    let r = 0,
+        g = 0,
+        b = 0;
+
+    r = Math.round(Math.random() * 255); //Math eh uma classe matematica
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+
+    this.setAttribute("style", `background-color: rgb(${r}, ${g},${b});`);
+});
+
+// <h2 class="tit-sec">Seçao de Fotos</h2>
+
+const h2Element = document.querySelector(".tit-sec");
+
+console.log(h2Element);
